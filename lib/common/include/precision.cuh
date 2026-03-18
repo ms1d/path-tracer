8e4188801e0a50be1caa6069acf1f3b50c93429a
Mahad Siddiqui <mahadsiddiqui07@outlook.com>
2026-03-18 16:57:54 +0000

    polish, added explicit compiler flags

--- lib/common/include/precision.cuh
+++ lib/common/include/precision.cuh
@@ -1,10 +1,10 @@
 #pragma once
 
 namespace math_precision {
-	constexpr float epsilon = 5e-6f;
+	constexpr float epsilon = 1e-6f;
 
-	__host__ __device__ bool nearly_equal(float a, float b) {
-		float rel_tol = 1e-4f;
+	__host__ __device__ constexpr bool nearly_equal(float a, float b) {
+		constexpr float rel_tol = 2e-6f;
 		return __builtin_fabs(a - b) <= __builtin_fmax(epsilon, rel_tol * __builtin_fmax(__builtin_fabs(a), __builtin_fabs(b)));
 	}
 }
